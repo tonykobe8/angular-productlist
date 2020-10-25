136794d950f8396dc288eb0cc6a5709f6958b8c5
tonykobe8 <tonykobe8@gmail.com>
2020-10-25 15:27:35 +0200

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule} from './app-routing.module';
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsListComponent implements OnInit {
 
   constructor() { }
-
+products = [{
+ image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/s-zoom.file", Name:'Portable monitor',Price:'R2,699',Description:'Asus MB168B 15.6'
+},{
+ image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/8806087691450_3-zoom.jpg", Name:'Curved monitor',Price:'R3,677',Description:'LG 25UM58-P Ultra Wild HD'
+}];
   ngOnInit() {
   }
 
